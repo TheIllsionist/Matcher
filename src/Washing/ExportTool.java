a5f30487b4db2ef8914245d47535d112750144f8
AwakeDreaming <awakedreaming@163.com>
2018-12-19 14:30:36 -0500

    加入数据导出和清洗简单类,后续会扩展

---
+++ src/Washing/ExportTool.java
@@ -0,0 +1,28 @@
+package Washing;
+
+import org.apache.jena.ontology.OntModel;
+
+import java.io.File;
+
+public class ExportTool {
+
+    /**
+     * 将知识库中每个类所拥有的属性持久化到文档里
+     * @param ontModel
+     * @param file
+     */
+    public static void exportPropsOfClses(OntModel ontModel,File file){
+
+    }
+
+    /**
+     * 将知识库中每个属性的定义域持久化到文档里
+     * @param ontModel
+     * @param file
+     */
+    public static void exportClsesOfProps(OntModel ontModel,File file){
+
+    }
+
+
+}
---
+++ src/Washing/WashTool.java
@@ -0,0 +1,13 @@
+package Washing;
+
+public class WashTool {
+
+    public static void washingDpVals(){
+
+    }
+
+    public static void addNewOpVals(){
+
+    }
+
+}
